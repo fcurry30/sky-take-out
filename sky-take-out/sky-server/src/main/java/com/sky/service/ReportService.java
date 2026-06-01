e576d40fa74fe00e5657ff363ec1bdbe89588a18
fcurry30 <543101743@qq.com>
2026-06-01 21:34:41 +0800

    营业额数据统计业务代码开发

---
+++ sky-take-out/sky-server/src/main/java/com/sky/service/ReportService.java
@@ -0,0 +1,15 @@
+package com.sky.service;
+
+import com.sky.vo.TurnoverReportVO;
+
+import java.time.LocalDate;
+
+public interface ReportService {
+    /**
+     * 根据日期统计营业额数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    TurnoverReportVO getTurnoverStatistics(LocalDate begin,LocalDate end);
+}
---
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -0,0 +1,62 @@
+package com.sky.service.impl;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import com.sky.service.ReportService;
+import com.sky.vo.TurnoverReportVO;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.util.StringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Slf4j
+public class ReportServiceImpl implements ReportService {
+    @Autowired
+    private OrderMapper orderMapper;
+    /**
+     * 根据日期统计营业额
+     * @param begin
+     * @param end
+     * @return
+     */
+    public TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end) {
+        //当前集合存放begin到end范围内的每天日期
+        List<LocalDate> dateList = new ArrayList<>();
+        dateList.add(begin);
+        while(!begin.equals(end)){
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }
+        //存放每天的营业额
+        List<Double> turnoverList = new ArrayList<>();
+        for (LocalDate date : dateList) {
+            //查询date日期对应的营业额数据，状态要为已完成
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+            //select sum(count) from orders where order_time > begin and order_time < end and status = 5;
+            Map<String,Object> map = new HashMap<>();
+            map.put("begin",beginTime);
+            map.put("end",endTime);
+            map.put("status", Orders.COMPLETED);
+            Double v = orderMapper.sumByMap(map);
+            v = v == null ? 0.0 : v;
+            turnoverList.add(v);
+        }
+        //拼接字符串，返回结果
+        String join = StringUtil.join(",", dateList);
+        String join1 = StringUtil.join(",", turnoverList);
+        TurnoverReportVO turnoverReportVO = new TurnoverReportVO();
+        turnoverReportVO.setDateList(join);
+        turnoverReportVO.setTurnoverList(join1);
+        return turnoverReportVO;
+    }
+}
